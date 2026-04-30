fadd66888f5f8d94e1f13d9a2d538d1a21f01d69
maxi <maxiferrarello@gmail.com>
2026-04-30 00:53:44 -0300

    commit

---
+++ Gestor_Salud-master/src/main/java/com/GrupoProga3/Gestor_Salud/GestorSaludApplication.java
@@ -0,0 +1,13 @@
+package com.GrupoProga3.Gestor_Salud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorSaludApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorSaludApplication.class, args);
+	}
+
+}
---
+++ Gestor_Salud-master/src/test/java/com/GrupoProga3/Gestor_Salud/GestorSaludApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GrupoProga3.Gestor_Salud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorSaludApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
