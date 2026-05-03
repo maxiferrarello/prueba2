a56894b0c5f685e8073ab5312ff2aa47b5b289eb
maxi <maxiferrarello@gmail.com>
2026-05-03 19:39:32 -0300

    esta listo

--- Gestor_Salud-master/src/main/java/com/GrupoProga3/Gestor_Salud/GestorSaludApplication.java
+++ Gestor_Salud-master/src/main/java/com/GrupoProga3/Gestor_Salud/GestorSaludApplication.java
@@ -19,12 +19,14 @@ public class GestorSaludApplication {
 
 
         System.out.println("estoy haciendo un commit y tengo que pasarlo a maxi x2");
-
-
+        
         System.out.println("estoy haciendo un commit y tengo que pasarlo a maxi x3254");
 
 
         System.out.println("chau estoy se borra?");
+
+
+        System.out.println("lo subo a main esto. esta listo!");
 
     }
 
