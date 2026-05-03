b3acc8484758889ef62615564cdd70be1b9ea525
maxi <maxiferrarello@gmail.com>
2026-05-03 19:37:19 -0300

    esto se borra?

--- Gestor_Salud-master/src/main/java/com/GrupoProga3/Gestor_Salud/GestorSaludApplication.java
+++ Gestor_Salud-master/src/main/java/com/GrupoProga3/Gestor_Salud/GestorSaludApplication.java
@@ -21,6 +21,9 @@ public class GestorSaludApplication {
         System.out.println("estoy haciendo un commit y tengo que pasarlo a maxi x2");
 
 
+        System.out.println("chau estoy se borra?");
+
+
 	}
 
 
